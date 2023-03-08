3d238fe9da4e62d571a9f7201cca130e130c48f0
kgul1 <113103268+kgul1@users.noreply.github.com>
2023-03-08 18:39:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 int nr_miesiaca;
 int  nr_roku_urodzenia;
 int nr_dnia_urodzenia;
-
+int parzyste(int i) {
+    if (nr_dnia_urodzenia % 2 == 0) {
+        cout << "ten dzien jest parzysty";
+    } else {
+        cout << "ten dzień nie jest parzysty";
+    }
+};
 int main() {
-    cout << "podaj dzien urodzenia";
+    cout << "podaj dzien urodzenia: ";
     if (!(cin >> nr_dnia_urodzenia)) {
         cout << "To nie jest liczba!";
         exit(0);
@@ -62,62 +68,36 @@ int main() {
     switch (nr_dnia_urodzenia) {
         case 1:
             cout << "\n poniadzialek"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
         case 2:
 
             cout << "\n wtorek"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
+
             break;
         case 3:
             cout << "\n sroda"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
         case 4:
             cout << "\n czwartek"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
         case 5:
             cout << "\n piatek"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
         case 6:
             cout << "\nsobota"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
         case 7:
             cout << "\nniedziela"<<endl;
-            if (nr_dnia_urodzenia % 2 == 0) {
-                cout << "ten dzien jest parzysty";
-            } else {
-                cout << "ten dzień nie jest parzysty";
-            }
+            parzyste(nr_dnia_urodzenia);
             break;
-
+        default:
+            cout << "Niepoprawny numer miesiaca!";
 
 
     }
